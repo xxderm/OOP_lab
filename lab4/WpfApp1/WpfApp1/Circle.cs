c5c36beef33981b61a39a781f1a1b116d1f50164
iilias <ptrwowhochy@gmail.com>
2022-11-01 19:46:31 +0500

    lab 4

---
+++ lab4/WpfApp1/WpfApp1/Circle.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace WpfApp1
+{    
+    public abstract class Shape
+    {
+        public void Select(Point point)
+        {
+            Unselect();
+            if (IsHover(point))
+                Selected = true;
+        }
+        public void Unselect()
+        {
+            Selected = false;
+        }
+        public abstract void Draw();
+        public abstract bool IsHover(Point point);        
+        public abstract string Name();
+        protected Point position;
+        protected Point Position { get => position; set => position = value; }
+        public bool Selected { get; private set; } = false;
+    }
+
+    public class Circle : Shape
+    {
+        public Circle(Canvas canvas, Point position, int radius)
+        {
+            this.position = position;
+            Radius = radius;
+            this.canvas = canvas;
+
+            Ellipse = new Ellipse
+            {
+                Name = "circle" + canvas.Children.Count.ToString()
+            };
+            canvas.Children.Add(Ellipse);
+        }
+
+        public override void Draw()
+        {
+            Ellipse.Width = Radius * 2;
+            Ellipse.Height = Radius * 2;
+            Ellipse.Stroke = defaultColor;
+            Ellipse.Fill = defaultColor;
+            if (Selected)
+            {
+                Ellipse.Stroke = selectColor;
+                Ellipse.Fill = selectColor;
+            }            
+            Ellipse.SetValue(Canvas.LeftProperty, position.X);
+            Ellipse.SetValue(Canvas.TopProperty, position.Y);
+        }
+
+        public override bool IsHover(Point point)
+        {
+            var circlePos = new Point(position.X + Radius, position.Y + Radius);
+            var dist = Point.Subtract(circlePos, point).Length;
+            return dist <= Radius;
+        }
+
+        public override string Name()
+        {
+            return Ellipse.Name;
+        }
+
+        private Canvas canvas;
+        private readonly Brush defaultColor = Brushes.Red;
+        private readonly Brush selectColor = Brushes.DarkRed;
+        public int Radius { get; set; }
+        public Ellipse Ellipse { get; private set; }
+    }
+}
---
+++ lab4/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Shape> shapes;
+        private bool select = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            shapes = new List<Shape>();
+            canvas.Focusable = true;
+            Loaded += (x, y) => Keyboard.Focus(canvas);
+        }
+
+        private Tuple<bool, Shape> GetHover()
+        {
+            for (int i = shapes.Count - 1; i >= 0; i--)
+            {
+                if (shapes[i].IsHover(Mouse.GetPosition(canvas)))
+                    return new Tuple<bool, Shape>(true, shapes[i]);
+            }
+            return new Tuple<bool, Shape>(false, null);
+        }
+
+        private void UnselectAll()
+        {
+            foreach (var shape in shapes)
+                shape.Unselect();
+        }
+
+        private void DrawAll()
+        {
+            foreach (var shape in shapes)
+                shape.Draw();
+        }
+
+        private void canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var pos = e.GetPosition(canvas);
+            var hoveredShape = GetHover();
+            if (!select)
+                UnselectAll();
+
+            if (hoveredShape.Item1)
+                hoveredShape.Item2.Select(pos);
+            else
+            {
+                pos.X -= 10;
+                pos.Y -= 10;
+                shapes.Add(new Circle(canvas, pos, 10));
+                shapes.Last().Select(e.GetPosition(canvas));
+            }
+
+            DrawAll();
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            label.Content = "";
+            var hoveredShape = GetHover();
+            if (hoveredShape.Item1)
+                label.Content = "Hover " + hoveredShape.Item2.Name();
+        }
+
+        private void canvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Delete:
+                    for (int i = 0; i < shapes.Count; i++)
+                    {
+                        if (shapes[i].Selected)
+                        {
+                            canvas.Children.RemoveAt(i + 1);
+                            shapes.RemoveAt(i); 
+                        }
+                    }
+                    break;
+                case Key.LeftCtrl:
+                    select = true;
+                    break;
+                default:
+                    break;
+            }
+            DrawAll();
+        }
+
+        private void canvas_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.LeftCtrl)
+                select = false;
+        }
+    }
+}
